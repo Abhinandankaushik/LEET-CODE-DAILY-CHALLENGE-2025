8b413181d55687864aa83b566e78abf3043cddfd
Abhinandan Kaushik <158504394+Abhinandankaushik@users.noreply.github.com>
2025-03-12 01:21:42 +0530

    Update (LC : 1358)_Number of Substrings Containing All Three Characters.cpp

--- (LC : 1358)_Number of Substrings Containing All Three Characters.cpp
+++ (LC : 1358)_Number of Substrings Containing All Three Characters.cpp
@@ -1,1 +1,29 @@
 
+class Solution {
+public:
+    bool isOkay(vector<int>& freq) {
+        return freq[0] > 0 && freq[1] > 0 && freq[2] > 0;
+    }
+    int numberOfSubstrings(string s) {
+
+        int n = s.length();
+        int i = 0, j = 0;
+        int ans = 0;
+
+        vector<int> freq(3, 0); //S.C : O(3) --> constant
+        
+        while (j < n) { //T.C : O(n)
+            char ch = s[j];
+
+            freq[ch - 'a']++;
+
+            while (isOkay(freq)) {
+                ans += (n - j);
+                freq[s[i] - 'a']--;
+                i++;
+            }
+            j++;
+        }
+        return ans;
+    }
+};
