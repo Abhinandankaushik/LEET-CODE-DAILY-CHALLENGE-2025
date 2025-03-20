33575c2f3cda4145edf5ebbdbf7640ecd42987c0
Abhinandan Kaushik <158504394+Abhinandankaushik@users.noreply.github.com>
2025-03-20 23:27:31 +0530

    Update (LC : 3108)_Minimum Cost Walk in Weighted Graph.cpp

--- (LC : 3108)_Minimum Cost Walk in Weighted Graph.cpp
+++ (LC : 3108)_Minimum Cost Walk in Weighted Graph.cpp
@@ -1,1 +1,62 @@
 
+class Solution {
+public:
+
+     vector<int>parent;
+     int find(int x){
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+     }
+
+     void Union(int x , int y){
+        parent[y] = x;
+     }
+
+    vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
+        
+        vector<int>cost(n);
+        parent.resize(n);
+        for(int i = 0 ;i < n ;i++){
+            parent[i] = i;
+            cost[i] = -1;
+        }
+
+
+        for(auto& edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+
+            int parent_u = find(u);
+            int parent_v = find(v);
+
+            if(parent_u != parent_v){
+
+                Union(parent_u,parent_v);
+                cost[parent_u] &= cost[parent_v];
+            }
+
+            cost[parent_u] &= w;
+        }
+
+         vector<int>result;
+        for(auto &it : query){
+             
+             int start = it[0];
+             int end = it[1];
+
+             int parent_u = find(start);
+             int parent_v = find(end);
+
+             if(parent_u != parent_v){
+                result.push_back(-1);
+             }
+             else{
+                result.push_back(cost[parent_u]);
+             }
+        }
+
+        return result;
+
+    }
+};
