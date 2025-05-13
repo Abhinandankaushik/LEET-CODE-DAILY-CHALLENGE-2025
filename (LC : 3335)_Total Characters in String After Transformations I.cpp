ce29406b0c194eca0ff89b23bc21a28724a5f6a6
Abhinandan Kaushik <abhinandankaushik.official@gmail.com>
2025-05-14 00:49:45 +0530

    Update (LC : 3335)_Total Characters in String After Transformations I.cpp

--- (LC : 3335)_Total Characters in String After Transformations I.cpp
+++ (LC : 3335)_Total Characters in String After Transformations I.cpp
@@ -1,1 +1,39 @@
 
+class Solution {
+public:
+    int lengthAfterTransformations(string s, int t) {
+
+       vector<int>v(26);
+        for (auto& it : s) { //T.C : O(n)
+            v[it-'a']++;
+        }
+        int mod = 1e9 + 7;
+         
+
+        for (int i = 0; i < t; i++) { //T.C : O(t)
+
+               vector<int>temp(26,0);
+
+            for (int j = 0 ; j < 26 ; j++) { //T.C : O(26)
+              
+                int freq = v[j];
+
+                if (j+1 != 26) {
+                    temp[j+1] = (temp[j + 1] + freq) % mod;
+
+                } else {
+                    temp[0] = (temp[0] + freq) % mod;
+                    temp[1] = (temp[1] + freq) % mod;
+                }
+            }
+
+            v = move(temp);
+        }
+
+        int ans = 0;
+        for (auto& it : v) {
+            ans = (ans + it ) % mod;
+        }
+        return ans;
+    }
+};
