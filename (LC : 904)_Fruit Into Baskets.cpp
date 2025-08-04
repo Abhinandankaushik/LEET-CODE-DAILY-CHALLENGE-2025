c37153abd39a59a81d35429f713d0451c7cc3235
Abhinandan Kaushik <abhinandankaushik.official@gmail.com>
2025-08-05 01:03:48 +0530

    Create (LC : 904)_Fruit Into Baskets.cpp

---
+++ (LC : 904)_Fruit Into Baskets.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+
+        int n = fruits.size();
+        int i = 0, j = 0;
+
+        map<int, int> mp;
+        int ans = -1;
+        while (j < n) {
+
+            mp[fruits[j]]++;
+            while (i < j && mp.size() > 2) {
+              
+
+                mp[fruits[i]]--;
+                if (mp[fruits[i]] == 0) {
+                    mp.erase(fruits[i]);
+                }
+                i++;
+            }
+              ans = max(ans,(j-i+1));
+            j++;
+        }
+        
+
+        return ans;
+    }
+};
